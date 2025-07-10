522f3cad9b1f297049bea7a0f535863c95a01de7
AbdulWahab17552 <abdulwahab22556@gmail.com>
2025-07-10 17:16:10 +0500

    new changes made

--- cypress/e2e/practice.cy.js
+++ cypress/e2e/practice.cy.js
@@ -2,7 +2,24 @@ describe('Practice Website', () => {
     it("Practice Website", () => {
         cy.visit("https://www.ropstam.com/")
         cy.get('.mobile_menu_bar').click()
-        cy.get('.body > div:nth-child(4) > div:nth-child(1) > header:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click()
+        cy.get('#mobile_menu1 > .et_pb_menu_page_id-914 > [href="https://www.ropstam.com/services/"]').click()
+        cy.get('#input_1_1').type('Abdul Wahab')
+        cy.get('#input_1_3').type('dkasjdkjdklasjdkksdj')
+        cy.get('#input_1_4').type('dksdkslkdlkd;ask')
+        cy.get('#input_1_5').type('CortechSols')
+        cy.get('#input_1_6').type('Lorem ipsum qwerty qwerty')
+        cy.get('#gform_submit_button_1').click()
+         cy.on('Windows:alert', (t) => {
+            expect(t).to.contains('Please fix the required fields with accurate information!');
+         cy.get('.gform_validation_errors').should('have.Text','Please fix the required fields with accurate information!')
+            
+        })
+       
     })
-})
+    // it('Practice',()=>{
+    //    cy.visit('https://www.ropstam.com/"')
+    // })
+  
 
+
+})
